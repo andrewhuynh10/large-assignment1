d7a9959d0a0495913783b9c50b9914b542d45e11
Anthony Martin <anthonym7@arizona.edu>
2025-02-24 17:26:31 -0700

    Added basic implemntation of Album.java

---
+++ src/model/Album.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Album{
+    private String title;
+    private String artist;
+    private String genre;
+    private int year;
+    private List<Song> songs;
+
+
+    public Album(String title, String artist, String genre, int year){
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.year = year;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public List<Song> getSongs(){
+        return Collections.unmodifiableList(songs);
+    }
+
+}
