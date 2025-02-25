140ed0b0578c3b02b2820a5028d079777d8636af
Anthony Martin <anthonym7@arizona.edu>
2025-02-24 17:27:15 -0700

    Added basic implemetation of Song.java

---
+++ src/model/Song.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Song{
+    private String title;
+    private String artist;
+    private String album;
+    private Rating rating;
+
+
+    public Song(String title, String artist, String album){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.rating = null;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public String getAlbum(){
+        return album;
+    }
+
+    public Rating getRating(){
+        return rating;
+    }
+
+    public void setRating(Rating rating){
+        this.rating = rating;
+    }
+}
