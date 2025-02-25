158cadfd46ae9783ae55c9b6c92608f90cdeabc5
Anthony Martin <anthonym7@arizona.edu>
2025-02-24 17:26:56 -0700

    Added basic implemetation of Playlist.java

---
+++ src/model/Playlist.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Playlist {
+    private String name;
+    private List<Song> songs;
+
+    public Playlist(String name){
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
+
+    public void addSong(Song song){
+        songs.add(song);
+    }
+
+    public boolean removeSong(Song song){
+        return songs.remove(song);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public List<Song> getSongs(){
+        return Collections.unmodifiableList(songs);
+    }
+
+    @Override
+    public String toString(){
+        return "Playlist: " + name + "\n Songs:" + songs;
+    }
+}
